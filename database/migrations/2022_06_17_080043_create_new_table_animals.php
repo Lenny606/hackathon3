d91ff7fe80ebba96acd24b85039f534ff08fba99
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-06-17 10:08:04 +0200

    table imported

---
+++ database/migrations/2022_06_17_080043_create_new_table_animals.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('image_id')->unsigned()->nullable();            
+            $table->bigInteger('owner_id')->nullable();
+            $table->text('name')->nullable();
+            $table->text('species')->nullable();
+            $table->text('breed')->nullable();
+            $table->integer('age')->nullable();
+            $table->integer('weight')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+};
