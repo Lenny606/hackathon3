7e353014e842992c09fc82819fdbfdac86c76de6
Blake Dodgen <blakedodgen@gmail.com>
2022-06-17 12:49:50 +0200

    html and styles

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,23 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HOME PAGE</title>
+    <title>St. Hector's Veterinary Clinic - Home</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <form action="" method="get">
-        <input type="text">
-        <input type="submit" value="Search">
-    </form>
-    <ul>
-    <?php foreach ($animals as $value): ?>
-        
-    <li>name: <?=$value->name?> </li>
-   
-    <?php endforeach; ?>
-    
- </ul>
+    <nav class="nav">
+
+        <div class=logo>
+            <img src="img/vetlogo.png" />
+            <h2 class="h2">ST. HECTOR'S VETERINARY CLINIC</h2>
+        </div>
+
+        <form action="" method="get">
+            <input type="text">
+            <input type="submit" value="Search">
+        </form>
+
+    </nav>
+    <div class="main">
+        <ul>
+            <?php foreach ($animals as $value): ?>
+
+            <li>name:
+                <?=$value->name?>
+            </li>
+
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <footer>
+        <p>© St. Hector's Veterinary Clinic 2022</p>
+        <footer>
 </body>
+
 </html>
